d36059aa59b3175117ccd8f7940f60cad51de87a
unknown <stephanievillegas0728@gmail.com>
2019-09-17 16:09:30 -0700

    main py modified

---
+++ PyBank/Main.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+import numpy as np
+import csv
+
+
+df = pd.read_csv("budget_data.csv")
+df
+
+
+print("Final Analysis")
+count1 = df['Date'].count()
+sum1 = df['Profit/Losses'].sum()
+mean1 = df['Profit/Losses'].mean()
+max1 = df['Profit/Losses'].max()
+min1 = df['Profit/Losses'].min()
+
+print('Total Months: ' + str(count1))
+print('Total: ' + str(sum1))
+print('Average Change: ' + str(mean1))
+print('Greatest Increase In Profits:Feb-2012 ' + str(max1))
+print('Greatest Decrease In Profits: Sep-2013 ' + str(min1))
---
+++ PyPoll/Main.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+import numpy as np
+import csv
+
+
+df = pd.read_csv (r"C:\Users\Stephanie Villegas\Documents\UA-PHX-DATA-PT-08-2019-U-C\02-Homework\Resources\PyPoll_Resources_election_data.csv")
+print (df)
+
+
+
+
+total_votes = len(df.index)
+count = df.Candidate.value_counts()
+pd.options.display.float_format = '{:.2f}%'.format
+percent = (df.Candidate.value_counts()/len(df.index))*100
+df2 = pd.DataFrame({'Percent': percent, 'Count': count})
+winner = df.Candidate.mode().values
+
+print("Election Results")
+print('-----------------------------')
+print('Total Votes: ' + str(total_votes))
+print('-----------------------------')
+print(df2.to_string(header=None))
+print('-----------------------------')
+print('Winner' + str(winner))
+print('-----------------------------')
+
